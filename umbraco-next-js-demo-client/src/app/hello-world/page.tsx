9572b147b52148df6fc531978288028a7452bdc9
LFLaverty <liamlaverty@gmail.com>
2024-02-29 10:06:36 +0000

    Adds a simple hello-world page

---
+++ umbraco-next-js-demo-client/src/app/hello-world/page.tsx
@@ -0,0 +1,13 @@
+
+const HelloWorld = async() => {
+  return (
+    <main className="flex min-h-screen flex-col p-24">
+       <h1 className="text-3xl mb-4">Hello World</h1>
+
+    </main>
+  );
+}
+
+
+
+export default HelloWorld;
