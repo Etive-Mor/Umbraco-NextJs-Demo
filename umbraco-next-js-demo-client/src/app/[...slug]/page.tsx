673b2718d4b793f40f31ae2b89eeed1fa6b3f9d6
LFLaverty <liamlaverty@gmail.com>
2024-02-29 10:17:12 +0000

    Adds nested-slug files

--- umbraco-next-js-demo-client/src/app/[...slug]/page.tsx
+++ umbraco-next-js-demo-client/src/app/[...slug]/page.tsx
@@ -3,15 +3,14 @@ import { GetPageAsync } from "@/services/services.umbraco/services.umbraco.conte
 
 const page = async ({ params }: { params: any }) => {
     console.log('slug: ' + JSON.stringify(params.slug))
-    const thisPage = await GetPageAsync(params.slug);
+    const thisPage = await GetPageAsync(params.slug.join('/'));
 
 
     return (
         <>
             <main className="flex min-h-screen flex-col p-24">
                 <h1 className="text-3xl mb-4">{thisPage.name}</h1>
-                <p>This page is a dynamic [slug]</p>
-
+                <p>This page is a dynamic [...slug]</p>
 
             </main>
         </>
